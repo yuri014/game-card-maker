c19c1178cf58fc9490fc377f47fb1896004c573a
yuri014 <yuriflores2012@gmail.com>
2020-09-07 21:05:52 -0300

    adding route for profile page

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('index');
+Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
