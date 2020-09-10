7d0212a957c26793b4792b9266d260204b9106c9
yuri014 <yuriflores2012@gmail.com>
2020-09-10 02:01:48 -0300

    adding edit profiles

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -7,11 +7,43 @@ use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    public function index($user) 
+    public function index($user)
     {
         $user = User::findOrFail($user);
-        return view('profile', [
+        return view('profiles.index', [
             'user' => $user
         ]);
+    }
+
+    public function edit(\App\User $user)
+    {
+        $this->authorize('update', $user->profile);
+
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $this->authorize('update', $user->profile);
+
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'link' => 'url',
+            'favoriteCardGame' => 'required',
+            'image' => ''
+        ]);
+
+        if (request('image')) {
+            $imagePath = request('image')->store('profile', 'public');
+            $imageArray = ['image' => $imagePath];
+        }
+
+        auth()->user()->profile->update(array_merge(
+            $data, 
+            $imageArray ?? []
+        ));
+
+        return redirect("/profile/$user->id");
     }
 }
