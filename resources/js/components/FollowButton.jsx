c8229d5f8910b6f11c98d81f7718d5da1d030292
yuri014 <yuriflores2012@gmail.com>
2020-09-11 00:29:45 -0300

    create react component to follow button

---
+++ resources/js/components/FollowButton.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function FollowButton(props) {
+    const [followState, setFollowState] = useState(props.follows);
+
+    function followUser() {
+        axios
+            .post(`/follow/${props.userId}`)
+            .then(response => {
+                setFollowState(!props.follows);
+            })
+            .catch(err => {
+                if (err.response.status == 401) {
+                    window.location = "/login";
+                }
+            });
+    }
+
+    return (
+        <button onClick={() => followUser()} className="btn btn-primary">
+            {!followState ? "Follow" : "Unfollow"}
+        </button>
+    );
+}
+
+export default FollowButton;
+
+const idElement = document.getElementById("follow-button");
+
+if (idElement) {
+    const userId = idElement.getAttribute("userId");
+    const follows = idElement.getAttribute("follows");
+    ReactDOM.render(
+        <FollowButton userId={userId} follows={follows} />,
+        document.getElementById("follow-button")
+    );
+}
