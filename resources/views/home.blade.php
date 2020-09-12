9d9c97645050f833a7a3bfb20084f745830ac919
yuri014 <yuriflores2012@gmail.com>
2020-09-12 02:57:29 -0300

    adding timeline posts to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,38 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            @foreach($posts as $post)
+            <div class="timeline-content">
+                <div class="timeline-profile-social">
+                    <div class="timeline-profile-image">
+                        <img src="{{ $post->user->profile->profileImage() }}" alt="User">
+                    </div>
+                    <div class="profile-details">
+                        <h5>{{ $post->user->username }}</h5>
+                        <a href="/profile/{{ $post->user->id }}" target="_blank"><i class="fas fa-link"></i></a>
+                    </div>
+                </div>
+                <div class="timeline-posts">
+                    <h4 class="post-title">{{ $post->title }}</h4>
+                    <div class="timeline-post">
+                        <img src="/storage/{{ $post->image }}" alt="">
+                        <div class="post-atributes">
+                            <div class="post-stats">
+                                <div class="damage">
+                                    <i class="fas fa-skull-crossbones"></i> {{ $post->power }}
+                                </div>
+                                <div class="defence">
+                                    <i class="fas fa-shield-alt"></i> {{ $post->defence }}
+                                </div>
+                            </div>
+                            <div class="post-description">
+                                {{ $post->description }}
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
 </div>
